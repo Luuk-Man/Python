1a62d98b738aaf39125fa95b2b1aabcc791ede51
Luuk_Man <79173354+Luuk-Man@users.noreply.github.com>
2022-06-28 19:36:10 +0200

    Update factorial.py

--- factorial.py
+++ factorial.py
@@ -1,8 +1,8 @@
 def main():
     x = int(input("The factorial of which number? "))
     y=1
-    for i in range (2, x + 1):
-         y *= i
+    for i in range (2, x + 1): #cycles through every number, except 1.
+         y *= i                #multiplies every number that it cycles through
     print(y)
     a = input("PRESS [ENTER] for relaunch")
     if a == "":
