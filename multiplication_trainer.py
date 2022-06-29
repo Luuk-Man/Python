ab3c76dc0a00b895a5a37a3f9746f132a0705b30
Luuk_Man <79173354+Luuk-Man@users.noreply.github.com>
2022-06-29 17:37:55 +0200

    a trainer for multiplication

---
+++ multiplication_trainer.py
@@ -0,0 +1,27 @@
+from random import*
+def main():
+    good = 0
+    bad = 0
+    lowest = int(input("What's the lowest?: "))
+    highest = int(input("What's the highest?: "))
+    times = int(input("how many times?: "))
+    for i in range (0,times):
+        m1 = randrange(lowest,highest)
+        m2 = randrange(lowest,highest)
+        print('What is',m1,'times',m2,'?')
+        ans = int(input(""))
+        if ans == m1 * m2:
+            print("Good Job")
+            good+=1
+        else: 
+            print("not quite!")
+            print("the answer should be:", m1*m2)
+            bad+=1
+    print(bad, "bad answerds given")
+    print(good,"good answers given")
+    repeat = input("PRESS [ENTER] TO PLAY AGAIN!")
+    if repeat == "":
+        main()
+    else:
+        exit()
+main()
